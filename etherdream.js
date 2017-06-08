657750fadda7c07b41a2cb4bc503a863280fd509
Frederic Heem <frederic.heem@gmail.com>
2017-06-08 18:24:36 +0100

    new algo

--- etherdream.js
+++ etherdream.js
@@ -1,102 +1,126 @@
 #!/usr/bin/env node
-import _ from 'lodash';
-import fs from 'fs';
-var Combinatorics = require('js-combinatorics');
+const _ = require("lodash");
+const fs = require("fs");
+var Combinatorics = require("js-combinatorics");
 
-function EtherDream(){
-  console.log('EtherDream');
-  let rEmail = RuleSymbol('MAXIME;BEYNET@GMAIL;COM');
-  let rEther = RuleOr(["ET", "ETHER", "ETHEREUM"]);
-  let rWallet = RuleOr(["wallet", "WALLET"]);
-  let rNum = RuleOr(["2", "02"])
-
-  let rWalletNum = RuleAnd(rWallet, rNum);
-
-  let seps = ['', '-', '_', ' '];
-  let finalResult = _.flatten(_.map(seps, sep => {
-    //let rSep = RuleSymbol(sep)
-    let rSep = RuleOr(['', sep])
-    let rCodes = RulesPermutation(["vt88q6s2", "yp3s532g", "Y@", "ET"], rSep);
-    let rules = [rCodes, rEther, rWalletNum, rEmail]
-    let rulesComb = Combinatorics.permutationCombination(rules).toArray();
-    rulesComb = _.reject(rulesComb, rules => rules.length <= 4)
-  //console.log('combination: ', permComb);
-    let allCombinations = _.flatten(_.map(rulesComb, rules => {
-        //console.log('rules: ', rules);
-        return _.reduce(rules, (acc, rule, key) => applyRules(acc, rule, key < rules.length - 1 ? rSep : RuleSymbol('')), [''])
-    }))
-
-    return allCombinations;
-  }))
-
-  //console.log('#result ', finalResult.length);
-
-  console.log(finalResult);
-  //let finalResult = _.flatten(allCombinations)
-  console.log('#result ', finalResult.length);
-  writeToFile(finalResult)
+function permute(inArray) {
+  return inArray.reduce((acc, combination, key) => {
+    if (key % 1000 === 0) {
+      console.log("permute ", acc.length, combination, combination.length, key);
+    }
+    return acc.concat(Combinatorics.permutation(combination).toArray());
+  }, []);
 }
 
-function writeToFile(result){
-    let content = _.reduce(result, (acc, item) => acc + item + '\n', '');
-    fs.writeFileSync('password.txt', content);
-}
+async function EtherDreamV2() {
+  const email = [
+    "maxime.beynet@gmail.com"
+    /*"MAXIME.BEYNET@GMAIL.COM",
+    "MAXIME;BEYNET@GMAIL;COM"*/
+  ];
+  const ether = ["ET", "ETHER", "ETHEREUM", "Ether", "Ethereum"];
+  const wallet = [
+    "wallet",
+    "WALLET",
+    "Wallet",
+    "wallets",
+    "WALLETS",
+    "Wallets"
+  ];
+  const num = ["2", "02"];
+  const pw1 = ["vt88q6s2"];
+  const pw2 = ["yp3s532g"];
+  const seps = ["", " ", "-", "_"];
 
-function applyItems(rule, items){
-  return _.flatten(_.map(items, item => rule.applyItem(item)));
-}
+  let cp = Combinatorics.cartesianProduct(
+    email,
+    ether,
+    wallet,
+    num,
+    pw1,
+    pw2
+  ).toArray();
 
-function applyRules(block, ...rules){
-  //console.log("rules: ", rules)
-  return _.reduce(rules, (acc, rule) => applyItems(rule, acc), block)
-}
+  console.log("cp length: ", cp.length);
+  console.log("cp: ", cp[0]);
 
-function RuleSymbol(symbol){
-  return {
-    symbol,
-     applyItem(item){
-       //console.log('RuleSymbol applyItem:', item);
-       return item + symbol
-     }
+  let result = permute(cp);
+
+  console.log("#result length ", result.length);
+  console.log("#result ", result[0]);
+  console.log("#result ", result[result.length - 1]);
+
+  /*
+  [ [ 'A' ],
+  [ '', ' ', '-', '_' ],
+  [ 'B' ],
+  [ '', ' ', '-', '_' ],
+  [ 'C' ] ]
+  */
+  //console.log(insertSeparator(["A", "B", "C"]));
+
+  function insertSeparator(comb) {
+    //console.log("comb ", comb);
+
+    return comb.reduce((acc, value, index) => {
+      acc.push([value]);
+      if (index < comb.length - 1) {
+        acc.push(seps);
+      }
+      return acc;
+    }, []);
   }
-}
-function RuleAnd(...rules){
-  return {
-     name: 'and',
-     applyItem(item){
-       //console.log('RuleAnd applyItem:', item);
-       return _.reduce(rules, (acc, rule) => applyItems(rule, acc), [item])
-     }
-  }
-}
 
-function RuleOr(choices){
-  return {
-    choices,
-     applyItem(item){
-       return _.map(choices, blockItem => item + blockItem)
-     }
+  async function createStream(name) {
+    return new Promise(resolve => {
+      var stream = fs.createWriteStream(name);
+      stream.once("open", function(fd) {
+        resolve(stream);
+      });
+    });
   }
-}
 
-function RulesPermutation(itemsIn, seperator){
-  let permComb = Combinatorics.permutationCombination(itemsIn).toArray();
-  permComb = _.reject(permComb, combination => combination.length < 2);
-  //console.log("RulesPermutation ", permComb);
-  let allCombinations = _.flatten(_.map(permComb, items => {
-    //console.log("items ", items);
-      return _.reduce(items, (acc, item, key) => {
-        //console.log("item %s, key %s, len %s, acc", item, key, items.length, acc);
-        return applyRules(acc, RuleSymbol(item), key < items.length - 1 ? seperator : RuleSymbol(''))
-      }, ['']);
-  }))
-  console.log(allCombinations);
-  return {
-    name: "permutation",
-    applyItem(item){
-      return _.map(allCombinations, combinations => item + combinations)
+  let stream;
+  let fileNumber = 0;
+  const itemsPerFile = 10000000;
+
+  async function getStream(count) {
+    if (count >= fileNumber * itemsPerFile) {
+      fileNumber++;
+      console.log("create new file ", fileNumber);
+      stream && stream.end();
+      stream = await createStream(`password-${fileNumber}.txt`);
+    }
+    return stream;
+  }
+
+  async function finalCp(results) {
+    let itemCount = 0;
+    for (let i = 0; i < results.length; i++) {
+      const content = Combinatorics.cartesianProduct(...insertSeparator(results[i]))
+        .toArray()
+        .map(value => value.join(""));
+
+      if (i % 1000 === 0) {
+        console.log("", itemCount, i);
+        console.log(
+          "finalCp",
+          content[0]
+        );
+      }
+      const currentStream = await getStream(itemCount);
+      currentStream.write(content.reduce((acc, item) => acc + item + "\n", ""));
+      itemCount += content.length;
     }
   }
-}
 
-EtherDream()
+  await finalCp(result);
+  stream.end();
+
+
+  console.log("#result length ", result.length);
+  console.log("#result ", result[0]);
+  console.log("#result ", result[result.length - 1]);
+}
+EtherDreamV2();
+
